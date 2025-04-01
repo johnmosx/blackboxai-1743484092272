e0f639ef22ab58b3a9229363493e3b4caed52c1f
johnmosx <49607529+johnmosx@users.noreply.github.com>
2025-04-01 11:07:45 +0300

    Add agri-crm/server/routes/userRoutes.js 1743494865354

--- agri-crm/server/routes/userRoutes.js
+++ agri-crm/server/routes/userRoutes.js
@@ -2,6 +2,7 @@ const express = require('express');
 const router = express.Router();
 const userController = require('../controllers/userController');
 const authMiddleware = require('../middlewares/authMiddleware');
+const roleMiddleware = require('../middlewares/roleMiddleware');
 
 // Apply auth middleware to all routes
 router.use(authMiddleware);
@@ -10,5 +11,6 @@ router.use(authMiddleware);
 router.post('/', userController.createUser);
 router.get('/', userController.getUsers);
 router.post('/change-password', userController.changePassword);
+router.put('/:id', roleMiddleware('Administrator'), userController.updateUser);
 
 module.exports = router;
