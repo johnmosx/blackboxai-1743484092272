5501e853e8d2e754157450a924102bdb7b9d4e75
johnmosx <49607529+johnmosx@users.noreply.github.com>
2025-04-01 09:52:44 +0300

    Add agri-crm/client/src/App.js 1743490363726

--- agri-crm/client/src/App.js
+++ agri-crm/client/src/App.js
@@ -1,27 +1,26 @@
 import { Routes, Route } from 'react-router-dom';
 import 'bootstrap/dist/css/bootstrap.min.css';
+import Layout from './components/Layout';
 import Dashboard from './pages/Dashboard';
 import Farmers from './pages/Farmers';
 import FarmerForm from './components/FarmerForm';
 import FarmerView from './components/FarmerView';
+import Login from './pages/Login';
 import Crops from './pages/Crops';
 import Reports from './pages/Reports';
-import Login from './pages/Login';
-import Layout from './components/Layout';
 
 function App() {
   return (
     <Routes>
       <Route path="/login" element={<Login />} />
-      <Route path="/" element={<Layout />}>
-        <Route index element={<Dashboard />} />
-        <Route path="farmers" element={<Farmers />} />
-        <Route path="farmers/new" element={<FarmerForm />} />
-        <Route path="farmers/:id" element={<FarmerForm />} />
-        <Route path="farmers/view/:id" element={<FarmerView />} />
-        <Route path="crops" element={<Crops />} />
-        <Route path="reports" element={<Reports />} />
-      </Route>
+      <Route path="/login" element={<Login />} />
+      <Route path="/" element={<Layout><Dashboard /></Layout>} />
+      <Route path="/farmers" element={<Layout><Farmers /></Layout>} />
+      <Route path="/farmers/new" element={<Layout><FarmerForm /></Layout>} />
+      <Route path="/farmers/:id" element={<Layout><FarmerForm /></Layout>} />
+      <Route path="/farmers/view/:id" element={<Layout><FarmerView /></Layout>} />
+      <Route path="/crops" element={<Layout><Crops /></Layout>} />
+      <Route path="/reports" element={<Layout><Reports /></Layout>} />
     </Routes>
   );
 }
