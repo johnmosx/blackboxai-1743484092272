87ed1b096220c6f2e6a955fe502df20979d20359
johnmosx <49607529+johnmosx@users.noreply.github.com>
2025-04-01 08:15:59 +0300

    Add agri-crm/client/src/App.js 1743484559435

--- agri-crm/client/src/App.js
+++ agri-crm/client/src/App.js
@@ -1,6 +1,8 @@
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Dashboard from './pages/Dashboard';
 import Farmers from './pages/Farmers';
+import FarmerForm from './components/FarmerForm';
+import FarmerView from './components/FarmerView';
 import Crops from './pages/Crops';
 import Reports from './pages/Reports';
 import Login from './pages/Login';
@@ -14,6 +16,9 @@ function App() {
         <Route path="/" element={<Layout />}>
           <Route index element={<Dashboard />} />
           <Route path="farmers" element={<Farmers />} />
+          <Route path="farmers/new" element={<FarmerForm />} />
+          <Route path="farmers/:id" element={<FarmerForm />} />
+          <Route path="farmers/view/:id" element={<FarmerView />} />
           <Route path="crops" element={<Crops />} />
           <Route path="reports" element={<Reports />} />
         </Route>
