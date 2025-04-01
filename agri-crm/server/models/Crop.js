c94581e5da4a2cfb36b9c43ac4330819713d5475
johnmosx <49607529+johnmosx@users.noreply.github.com>
2025-04-01 09:53:04 +0300

    Add agri-crm/server/models/Crop.js 1743490383991

---
+++ agri-crm/server/models/Crop.js
@@ -0,0 +1,24 @@
+module.exports = (sequelize, DataTypes) => {
+  const Crop = sequelize.define('Crop', {
+    name: {
+      type: DataTypes.STRING,
+      allowNull: false
+    },
+    plantingDate: DataTypes.DATE,
+    harvestDate: DataTypes.DATE,
+    status: {
+      type: DataTypes.ENUM,
+      values: ['planted', 'growing', 'harvested'],
+      defaultValue: 'planted'
+    }
+  });
+
+  Crop.associate = (models) => {
+    Crop.belongsTo(models.Farmer, {
+      foreignKey: 'farmerId',
+      onDelete: 'CASCADE'
+    });
+  };
+
+  return Crop;
+};
