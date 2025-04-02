3645b40fbae6923917db1c98970e1d8d3af68c5e
johnmosx <49607529+johnmosx@users.noreply.github.com>
2025-04-02 08:49:51 +0300

    Add agri-crm/server/routes/auth.js 1743572991416

--- agri-crm/server/routes/auth.js
+++ agri-crm/server/routes/auth.js
@@ -5,6 +5,5 @@ const authMiddleware = require('../middlewares/authMiddleware');
 
 router.post('/login', authController.login);
 router.get('/verify', authMiddleware, authController.verifyToken);
-router.get('/me', authMiddleware, authController.getCurrentUser);
 
 module.exports = router;
