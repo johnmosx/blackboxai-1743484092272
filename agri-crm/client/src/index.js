9db38542b7b30adf9300cf9d5de065cf2d21215b
johnmosx <49607529+johnmosx@users.noreply.github.com>
2025-04-03 09:15:49 +0300

    Add agri-crm/client/src/index.js 1743660948255

--- agri-crm/client/src/index.js
+++ agri-crm/client/src/index.js
@@ -5,6 +5,7 @@ import './index.css';
 import { AuthProvider } from './contexts/AuthContext';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import 'bootstrap/dist/css/bootstrap.css';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
