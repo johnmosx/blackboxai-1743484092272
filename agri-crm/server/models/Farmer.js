44534a88e11e0e97b27fb54faebf17d91d1f0286
johnmosx <49607529+johnmosx@users.noreply.github.com>
2025-04-03 12:01:43 +0300

    Add agri-crm/server/models/Farmer.js 1743670903167

--- agri-crm/server/models/Farmer.js
+++ agri-crm/server/models/Farmer.js
@@ -2,37 +2,21 @@
 const { Model } = require('sequelize');
 
 module.exports = (sequelize, DataTypes) => {
-  class Farmer extends Model {
-    static associate(models) {
-      // associations can be defined here
-    }
-  }
-  Farmer.init({
+  const Farmer = sequelize.define('Farmer', {
     name: {
       type: DataTypes.STRING,
       allowNull: false
     },
-    phone: {
-      type: DataTypes.STRING,
-      allowNull: false,
-      validate: {
-        is: /^[\+]?[(]?[0-9]{3}[)]?[-\s\.]?[0-9]{3}[-\s\.]?[0-9]{4,6}$/im
-      }
-    },
-    email: {
-      type: DataTypes.STRING,
-      allowNull: false,
-      validate: {
-        isEmail: true
-      }
-    },
-    location: {
-      type: DataTypes.STRING,
-      allowNull: false
-    }
-  }, {
-    sequelize,
-    modelName: 'Farmer',
+    phone: DataTypes.STRING,
+    email: DataTypes.STRING
   });
+
+  Farmer.associate = (models) => {
+    Farmer.hasMany(models.Field, {
+      foreignKey: 'farmerId',
+      as: 'fields'
+    });
+  };
+
   return Farmer;
 };
